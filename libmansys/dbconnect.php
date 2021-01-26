893ab3552be3b6aa2bcf8c790e5dfb68be9915cb
ikhwanmukhtar <73566125+ikhwanmukhtar@users.noreply.github.com>
2021-01-26 09:27:14 +0800

    Update dbconnect.php

--- libmansys/dbconnect.php
+++ libmansys/dbconnect.php
@@ -2,7 +2,7 @@
 
 $servername = "sql105.byetcluster.com";
 $username = "epiz_27510968";
-$password = "jTVoHaPT0NJ2s9QnaFyZi91f";
+$password = "PT0NJ2s9QnaFyZ";
 $database = "epiz_27510968_libmansys";
 
 $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
